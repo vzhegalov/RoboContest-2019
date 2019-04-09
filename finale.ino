2b31cca71002dbd73fa2d76bc649875ab31f85d1
vzhegalov <v.zhegalov@yandex.ru>
2019-04-09 19:32:59 +0200

    Update finale.ino

--- finale.ino
+++ finale.ino
@@ -2,17 +2,26 @@
 int IN3 = 5; // Input3 подключен к выводу 5 
 int IN4 = 4;
 int kn1;
+int kn2;
+int state;
 void setup()
 {
   pinMode (IN4, OUTPUT);
   pinMode (IN3, OUTPUT);
   pinMode (A0, INPUT);
+  pinMode (A1, INPUT);
   Serial.begin(9600);
 }
 void loop()
 {
  kn1 = digitalRead(A0);
+ kn2 = digitalRead(A1);
  Serial.println(kn1);
+ if(kn2 == 1){
+  digitalWrite (IN4, HIGH);
+  digitalWrite (IN3, LOW);
+ }
+ else
  if(kn1 == 1){ 
   digitalWrite (IN3, HIGH);
   digitalWrite (IN4, LOW);
